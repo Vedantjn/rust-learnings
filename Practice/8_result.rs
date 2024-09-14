6c374b63da0b87c91c5b10ebc0350a055ec67dac
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-14 13:08:32 +0530

    result

--- Practice/8_result.rs
+++ Practice/8_result.rs
@@ -1,10 +1,10 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let ans = read_from_file_kirat(String::from("a.txt"));
+    let ans = read_from_file(String::from("a.txt"));
 }
 
-fn read_from_file_kirat(file_path: String) -> Result<String, String> {
+fn read_from_file(file_path: String) -> Result<String, String> {
     let result = read_to_string(file_path); // Result
     match result {
         Ok(data) => Ok(data),
