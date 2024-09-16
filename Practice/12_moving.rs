babc11633deeceed54e3f2fb496a39176dbaf7fb
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-16 12:20:46 +0530

    moving

--- Practice/12_moving.rs
+++ Practice/12_moving.rs
@@ -1,12 +1,24 @@
+// fn main() {
+//     create_string();
+// }
+
+// fn create_string() {
+//     let s1 = String::from("Vedant");
+//     // let s2 = s1.clone();
+//     let s2 = s1;
+
+//     println!("{}", s1);
+//     println!("{}", s2);
+// }
+
+
 fn main() {
-    create_string();
+    let s1 = String::from("Vedant");
+    do_something(s1); // Pass ownership of s1 to do_something
+
+    // println!("{}", s1); // This will cause a compile-time error because s1 is moved
 }
 
-fn create_string() {
-    let s1 = String::from("Vedant");
-    // let s2 = s1.clone();
-    let s2 = s1;
-
-    println!("{}", s1);
+fn do_something(s2: String) {
     println!("{}", s2);
 }
