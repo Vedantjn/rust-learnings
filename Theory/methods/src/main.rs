b9db035d58d886f56d57cb1cade4414f7e610bac
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-17 09:54:29 +0530

    methods

--- Theory/methods/src/main.rs
+++ Theory/methods/src/main.rs
@@ -9,6 +9,10 @@ impl Rectangle {
     fn calculate_area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn main() {
@@ -16,6 +20,13 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    let rect2 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+
+    println!("Can rect1 hold rect2 ? {}", rect2.can_hold(&rect1));
 
     println!(
         "The area of the rectangle is {} square pixels.", rect1.calculate_area()
